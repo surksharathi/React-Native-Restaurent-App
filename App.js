f31c6c5868cf14eb2e35480df8ca9ef4a5618114
Suraksha Singh <surksha8@gmail.com>
2022-01-10 13:37:54 +0800

    app base setup

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
+
+import RestaurantsScreen from './src/features/restaurants/screens/restaurants.screen';
+
+export default function App() {
+   
+  return (
+    <>
+  <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
+     
+    </>
+  );
+}
+
