2bdf48057ca079026e2175d6f737e2197c8a07b5
Suraksha Singh <surksha8@gmail.com>
2022-01-12 18:16:05 +0800

    navigation is added in app

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -6,21 +6,13 @@ import { Searchbar } from "react-native-paper";
 import { StatusBar, StyleSheet, SafeAreaView, Text, View, FlatList, } from "react-native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from '../../../components/spacer/spacer.components';
+import { SafeArea } from "../../../components/utlity/safe-area.component";
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
- ${StatusBar.currentHeight}&& margin-top: ${StatusBar.currentHeight}px;
-`;
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding:  ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary}
-`;
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
